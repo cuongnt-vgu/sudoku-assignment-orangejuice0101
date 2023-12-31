a00a3107df4710aec689d5c014b8ce075e268b4d
orangejuice0101 <137214418+orangejuice0101@users.noreply.github.com>
2023-12-31 14:22:48 +0000

    final2

---
+++ src/cell.h
@@ -0,0 +1,12 @@
+#ifndef CELL_H
+#define CELL_H
+
+typedef struct {
+    int value;
+    int candidates[9];
+    int num_candidates;
+} Cell;
+
+// Function prototypes for functions related to cells (if any)
+
+#endif
